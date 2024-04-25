6fbe1f21110548184e2b15d73d41cb7cfe12373e
Irina-Piskokha <145346308+Irina-Piskokha@users.noreply.github.com>
2024-04-25 14:33:05 +0000

    Deploying to gh-pages from @ Irina-Piskokha/project_test@c689bfe349d610cf53abb71215a944317ad35593 🚀

--- assets/vendor-44c3de12.js
+++
@@ -1,2 +0,0 @@
-if(typeof window<"u"){var e={get passive(){}};window.addEventListener("testPassive",null,e),window.removeEventListener("testPassive",null,e)}
-//# sourceMappingURL=vendor-44c3de12.js.map
